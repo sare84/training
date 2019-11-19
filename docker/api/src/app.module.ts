27ef744500049a7cb51e209ae2f1c03e1980d217
Sascha Reichert <reichert.sascha@googlemail.com>
2019-11-19 19:49:56 +0100

    added middleware for logging

--- docker/api/src/app.module.ts
+++ docker/api/src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InfoController } from './info/info.controller';
 import { InfoService } from './info/info.service';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController, InfoController],
   providers: [AppService, InfoService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
